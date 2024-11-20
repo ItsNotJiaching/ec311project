46c3ce7ef29a8f6012af00ed68608b8ea8b0df1f
jianzhengr3591 <73479669+jianzhengr3591@users.noreply.github.com>
2024-11-19 19:41:12 -0500

    Update ball.v

--- ec311project.srcs/sources_1/new/ball.v
+++ ec311project.srcs/sources_1/new/ball.v
@@ -1,1 +1,32 @@
-//module ball();
+module ball(
+  input wire clk,
+  input wire reset,
+  input wire [9:0] screen_width,
+  input wire [9:0] screen_height,
+  output reg [9:0] ball_x,
+  output reg [9:0] ball_y
+);
+  reg [9:0] velocity_x;
+  reg [9:0] velocity_y;
+  initial begin 
+    ball_x = screen_width/2;
+    ball_y = screen_height/2;
+    velocity_x = 1;
+    velocity_y =1;
+  end
+  always @ (posedge clk or posedge reset) begin
+    if (reset) begin 
+      ball_x <= screen_width/2;
+      ball_y <= screen_height/2;
+      velocity_x = 1;
+      velocity_y = 1;
+    end else begin 
+      ball_x <= ball_x + velocity_x;
+      ball_y <= ball_y + velocity_y;
+      if ( ball_x <= 0 || ball_x >= screen_width -1 || ball_y <= 0|| ball_y >= screen_height-1)begin
+        ball_x <= screen_width/2;
+        ball_y <= screen_height/2;
+      end 
+    end 
+  end 
+endmodule 
