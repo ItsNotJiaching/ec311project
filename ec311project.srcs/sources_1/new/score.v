441dc9710c4014fec5d734b602d9f7311d7cbced
hannah 3 <47232511+hftandang@users.noreply.github.com>
2024-11-19 20:58:47 -0500

    Update score.v
    -m change

--- ec311project.srcs/sources_1/new/score.v
+++ ec311project.srcs/sources_1/new/score.v
@@ -1,19 +1,18 @@
-module score(input collision_flag1, input collision_flag2, output reg p_one_score, output reg p_two_score;);
+module score(input collision_flag1, input collision_flag2, output reg resetflag, output reg [2:0] p_one_score, output reg [2:0] p_two_score);
 
 
-initial p_two_score = 0;
+initial p_two_score = 3'b000;
+initial p_one_score = 3'b000;
 
-initial p_one_score = 0;
 
-
-always @ (collision_flag1) begin
-p_two_score <= p_two_score + 1;
-collision_flag1 <= 0;
+always @ (posedge collision_flag1) begin
+p_two_score <= (p_two_score + 3'b001);
+resetflag <= 1;
 end
 
-always @ (collision_flag2) begin
-p_one_score <= p_one_score + 1;
-collision_flag2 <= 0;
+always @ (posedge collision_flag2) begin
+p_one_score <= p_one_score + 3'b001;
+resetflag <= 1;
 end
 
 endmodule
