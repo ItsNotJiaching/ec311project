cf0f29a2ada32497b248c4e3fc0727a4fb248496
Ruan <jzruan@bu.edu>
2024-11-19 20:38:23 -0500

    hannahgit

--- ec311project.srcs/sources_1/new/ball.v
+++ ec311project.srcs/sources_1/new/ball.v
@@ -4,30 +4,31 @@ module ball(
   input wire reset,
   input wire [9:0] screen_width,
   input wire [9:0] screen_height,
+  input wire [9:0] velocity_x,
+  input wire [9:0] velocity_y,
   output reg [9:0] ball_x,
   output reg [9:0] ball_y,
-  output reg [9:0] velocity_x,
-  output reg [9:0] velocity_y
+  output reg edgeflag_left,
+  output reg edgeflag_right
 );
 
   initial begin 
     ball_x = screen_width/2;
     ball_y = screen_height/2;
-    velocity_x = 1;
-    velocity_y =1;
+
   end
   always @ (posedge clk or posedge reset) begin
     if (reset) begin 
       ball_x <= screen_width/2;
       ball_y <= screen_height/2;
-      velocity_x = 1;
-      velocity_y = 1;
+     // reset velocity at top level module 
     end else begin 
       ball_x <= ball_x + velocity_x;
       ball_y <= ball_y + velocity_y;
-      if ( ball_x <= 0 || ball_x >= screen_width -1 || ball_y <= 0|| ball_y >= screen_height-1)begin
+      if ( ball_x <= 0 || ball_x >= screen_width -1 )begin
         ball_x <= screen_width/2;
         ball_y <= screen_height/2;
+        // reminder you need to reset other things??? (reset score, reset position)
       end 
     end 
   end 
