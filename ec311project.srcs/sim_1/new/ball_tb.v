dff0a2915e8d2e0bbaf495008b6218c5a6b3d7d8
Ruan <jzruan@bu.edu>
2024-11-24 16:10:45 -0500

    finalupdate for ball

--- ec311project.srcs/sim_1/new/ball_tb.v
+++ ec311project.srcs/sim_1/new/ball_tb.v
@@ -23,20 +23,27 @@
 module ball_tb(
 
     );
-    reg clk;
-    reg reset;
-    reg [9:0] screen_width;
-    reg [9:0] screen_height;
-    wire [9:0] ball_x;
-    wire [9:0] ball_y;
-    ball testball (clk,reset,screen_width,screen_height,ball_x,ball_y);
+   reg clk;
+   reg resetflag;
+   reg [9:0] velocity_x;
+   reg [9:0] velocity_y;
+   wire [9:0] ball_x;
+   wire [9:0] ball_y;
+   wire edgeflag_left;
+   wire edgeflag_right;
+    ball testball(clk,resetflag,velocity_x,velocity_y,ball_x,ball_y,edgeflag_left,edgeflag_right);
     always #5 clk = ~clk;
     initial begin 
         clk = 0;
-        reset = 1;
-        screen_width = 640;
-        screen_height = 480;
-        #20 reset = 0;
+        resetflag = 1;
+        velocity_x = 2;
+        velocity_y = 2;
+        #20 resetflag = 0;
+        #20 velocity_x = 0;
+        #20 velocity_y = 0;
+        #15 resetflag = 1;
+        #20 velocity_x = 2;
+        #20 velocity_y = 2;         
         #50 $finish;
       end
 endmodule
--- ec311project.srcs/sources_1/new/ball.v
+++ ec311project.srcs/sources_1/new/ball.v
@@ -1,7 +1,7 @@
 `timescale 1ns / 1ps
 module ball(
   input wire clk,
-  input wire reset,
+  input wire resetflag,
   input wire [9:0] velocity_x,
   input wire [9:0] velocity_y,
   output reg [9:0] ball_x,
@@ -9,28 +9,39 @@ module ball(
   output reg edgeflag_left,
   output reg edgeflag_right
 );
-    parameter screen_width = 640;
-    parameter screen_height = 480;    
+    
+   parameter screen_width = 640;
+   parameter screen_height = 480;
   initial begin 
     ball_x = screen_width/2;
     ball_y = screen_height/2;
+    edgeflag_left = 0;
+    edgeflag_right = 0;
 
   end
-  always @ (posedge clk or posedge reset) begin
-    if (reset) begin 
+ always @ (posedge clk or posedge resetflag) begin
+    if (resetflag) begin 
       ball_x <= screen_width/2;
       ball_y <= screen_height/2;
-     // reset velocity at top level module 
+      edgeflag_left <= 0;
+      edgeflag_right <= 0;     // reset velocity at top level module 
     end else begin 
       ball_x <= ball_x + velocity_x;
       ball_y <= ball_y + velocity_y;
-      if ( ball_x <= 0 || ball_x >= screen_width -1 )begin
-        ball_x <= screen_width/2;
-        ball_y <= screen_height/2;
-        edgeflag_left <= 1; 
-        edgeflag_right <= 1;
-        // reminder you need to reset other things??? (reset score, reset position)
       end 
+      if ( ball_x <= 0 )begin
+        ball_x <= screen_width/2;
+        ball_y <= screen_height/2; 
+        edgeflag_left <= 1;
+        // reminder you need to reset other things??? (reset score, reset position)
+      end else begin 
+      if (ball_x >= screen_width -1) begin 
+        ball_x <= screen_width/2;
+        ball_y <= screen_height/2; 
+        edgeflag_right <= 1;
+      end
     end 
   end 
+ 
 endmodule 
+ 
