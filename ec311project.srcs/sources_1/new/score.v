3c1af2b6fc9b203fd51226307e610aea24527b64
hannah 3 <47232511+hftandang@users.noreply.github.com>
2024-11-24 16:22:37 -0500

    Update score.v

--- ec311project.srcs/sources_1/new/score.v
+++ ec311project.srcs/sources_1/new/score.v
@@ -1,21 +1,39 @@
-module score(input collision_flag1, input collision_flag2, output reg resetflag, output reg [2:0] p_one_score, output reg [2:0] p_two_score);
+module score(input collision_flag1, input collision_flag2, output reg resetflag, output reg [6:0] p_one_score, output reg [6:0] p_two_score);
 
 // collisionflag for each player if ball does not hit paddle (a point for the opposing player) 
 // all global var
 
 // initial score when game starts
-initial p_two_score = 3'b000;
-initial p_one_score = 3'b000;
+initial p_two_score = 7'b0000001;
+initial p_one_score = 7'b0000001;
+reg twoscore = 3'b000;
+reg onescore = 3'b000;
 
 
 always @ (posedge collision_flag1) begin
-p_two_score <= (p_two_score + 3'b001);
+twoscore <= (twoscore + 3'b001);
 resetflag <= 1; // if reset flag 1 need to reset collision flag in another var and set reset flag here to 0
+        case (twoscore)
+        3'b000: p_two_score = 7'b0000001; // "0"     
+        3'b001: p_two_score = 7'b1001111; // "1" 
+        3'b010: p_two_score = 7'b0010010; // "2" 
+        3'b011: p_two_score = 7'b0000110; // "3" 
+        3'b100: p_two_score = 7'b1001100; // "4" 
+        3'b101: p_two_score = 7'b0100100; // "5" 
+        endcase
 end
 
 always @ (posedge collision_flag2) begin
-p_one_score <= p_one_score + 3'b001;
+onescore <= onescore + 3'b001;
 resetflag <= 1;
+case (onescore)
+        3'b000: p_one_score = 7'b0000001; // "0"     
+        3'b001: p_one_score = 7'b1001111; // "1" 
+        3'b010: p_one_score = 7'b0010010; // "2" 
+        3'b011: p_one_score = 7'b0000110; // "3" 
+        3'b100: p_one_score = 7'b1001100; // "4" 
+        3'b101: p_one_score = 7'b0100100; // "5" 
+        endcase
 end
 
 endmodule
