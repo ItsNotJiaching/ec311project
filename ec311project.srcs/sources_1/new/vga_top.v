f0877576861a7155ca42e5d0233106cba46af7e4
Joey Sorkin <59318770+JoeySorkin@users.noreply.github.com>
2024-11-24 21:06:59 -0500

    Update vga_top.v

--- ec311project.srcs/sources_1/new/vga_top.v
+++ ec311project.srcs/sources_1/new/vga_top.v
@@ -26,15 +26,23 @@ module vga_top(clk, reset, vga_r, vga_g, vga_b, h_sync, v_sync);
 
     output reg [3:0] vga_r, vga_g, vga_b;
     output h_sync, v_sync;
-    wire newClk;
+    wire newClk, newClk2;
     wire [2:0] ledOn;
-    
+    wire [9:0] ball_pos_x, ball_pos_y;
+    wire edgeleft, edgeright;
     
   
     
     clk_divider clkDiv (clk, reset, newClk);
+    game_clock_divider clkDiv2 (clk, reset, newClk2);
     
-    vga_controller vga_con (newClk, h_sync, v_sync, ledOn);
+    
+    ball puck(newClk2, reset, 1, 0, ball_pos_x, ball_pos_y, edgeleft, edgeright);
+    
+    
+    vga_controller vga_con (newClk, h_sync, v_sync, ball_pos_x, ball_pos_y, ledOn);
+    
+    
     initial begin
         vga_r = 4'b0000;
         vga_g = 4'b0000;
